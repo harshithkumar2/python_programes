3ff4006577652850fdd933b53442f38b57f802f0
harshithkumar2 <harshithkumar40@gmail.com>
2021-05-03 20:04:26 +0530

    solved some solves

---
+++ find_elem.py
@@ -0,0 +1,22 @@
+def find_elem(num,key):
+    found = 0
+    for i in num:
+        if i == key:
+            found = 1
+            index = num.index(i)
+
+    if found:
+        print("Key found at position {0}".format(index))
+    else:
+        print("Key not found")
+
+if __name__ == "__main__":
+    numbers = []
+    num = int(input("Enter length"))
+    for _ in range(num):
+        nums = int(input())
+        numbers.append(nums)
+
+    key = int(input("Select key element"))
+
+    find_elem(numbers,key)
