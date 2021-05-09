fef1888da1f127c76a331dc1fa58ea5f207a61ed
adithyal-21 <adithya2120@gmail.com>
2021-05-09 22:26:03 +0530

    python programs

--- multiplication_table.py
+++
@@ -1,7 +0,0 @@
-def print_table(n):
-    for i in range(1,11):
-        print("{0} X {1} = {2}".format(n,i,n*i))
-
-if __name__ == "__main__":
-    N = int(input("Which multiplication table?"))
-    print_table(N)
--- right_tri_loop.py
+++
@@ -1,9 +0,0 @@
-def triangle(n):
-    for i in range(0,n):
-        for j in range(0,i):
-            print("*")
-
-
-if __name__ == "__main__":
-    n = int(input("Enter size"))
-    triangle(n)
